63bcc200a55564d4b7b058c39fcc57beacb5af4f
Sparsh865 <sparsh865.be22@chitkara.edu.in>
2024-03-24 11:38:37 +0530

    Edit Remove Btn added

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
 
-const inputBox= document.getElementById('inputBox');
+const inputBox = document.getElementById('inputBox');
 const addBtn = document.getElementById('addBtn');
 const todoList = document.getElementById('todoList');
+//global variable
+let editTodo = null;
 
-
-const addTodo = () =>{
+const addTodo = () => {
     const inputText = inputBox.value.trim();
-    if(inputText.length <= 0){
+    if (inputText.length <= 0) {
         alert("You must write something!!")
         return false;
     }
 
-    const li = document.createElement('li');
-    const p = document.createElement('p');
+    if (addBtn.value === 'Edit') {
+        editTodo.target.previousElementSibling.innerHTML = inputText;
+        addBtn.value = 'Add';
+        inputBox.value = "";
+    }
+    else {
 
-    p.innerHTML= inputText;
-    li.appendChild(p);
-    todoList.appendChild(li);
+        //Creating P tag
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.innerHTML = inputText;
+        li.appendChild(p);
+        todoList.appendChild(li);
 
-    inputBox.value="";//Empty todo box after adding
+        //Creating Edit Button
+        const editBtn = document.createElement('button');
+        editBtn.innerText = 'Edit';
+        editBtn.classList.add('btn', 'editBtn');
+        li.appendChild(editBtn);
 
+        //Creating Delete btn
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'Remove';
+        deleteBtn.classList.add('btn', 'deleteBtn')
+        li.appendChild(deleteBtn);
 
-    
-
+        //Empty todo box after adding
+        inputBox.value = "";
+    }
 }
-addBtn.addEventListener('click',addTodo);
+
+const updateTodo = (e) => {
+    if (e.target.innerHTML === 'Remove') {
+        todoList.removeChild(e.target.parentElement);
+    }
+
+    if (e.target.innerHTML === 'Edit') {
+        inputBox.value = e.target.previousElementSibling.innerHTML;
+        inputBox.focus();
+        addBtn.value = 'Edit';
+        editTodo = e;
+    }
+}
+
+addBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', updateTodo);
 
